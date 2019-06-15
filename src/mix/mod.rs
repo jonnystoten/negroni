d0c26738dec05e1bab96ea0d42a49c37e7f0d5f0
Jonny Stoten <jonny@jonnystoten.com>
2019-06-15 21:44:54 +0100

    Move opcodes into their own file

--- src/mix/mod.rs
+++ src/mix/mod.rs
@@ -1,6 +1,7 @@
 mod address;
 mod instruction;
 mod word;
+pub mod op_codes;
 
 pub use address::Address;
 pub use instruction::Instruction;
@@ -12,36 +13,4 @@ pub fn field_spec(left: u8, right: u8) -> u8 {
 
 pub fn decode_field_spec(spec: u8) -> (u8, u8) {
   (spec / 8, spec % 8)
-}
-
-pub mod op_codes {
-  pub const LDA: u8 = 8;
-  pub const LD1: u8 = 9;
-  pub const LD2: u8 = 10;
-  pub const LD3: u8 = 11;
-  pub const LD4: u8 = 12;
-  pub const LD5: u8 = 13;
-  pub const LD6: u8 = 14;
-  pub const LDX: u8 = 15;
-  pub const LDAN: u8 = 16;
-  pub const LD1N: u8 = 17;
-  pub const LD2N: u8 = 18;
-  pub const LD3N: u8 = 19;
-  pub const LD4N: u8 = 20;
-  pub const LD5N: u8 = 21;
-  pub const LD6N: u8 = 22;
-  pub const LDXN: u8 = 23;
-
-  pub const STA: u8 = 24;
-  pub const ST1: u8 = 25;
-  pub const ST2: u8 = 26;
-  pub const ST3: u8 = 27;
-  pub const ST4: u8 = 28;
-  pub const ST5: u8 = 29;
-  pub const ST6: u8 = 30;
-  pub const STX: u8 = 31;
-  pub const STJ: u8 = 32;
-  pub const STZ: u8 = 33;
-
-  pub const ENTA: u8 = 48;
 }
